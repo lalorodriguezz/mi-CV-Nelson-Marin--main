22231d3933a79ca4321ca4e7be40fd9e3c48c120
Nelson Eduardo Marín Rodríguez <81033388+lalorodriguezz@users.noreply.github.com>
2022-08-13 13:16:11 -0600

    Add files via upload

---
+++ java.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal('.left_Side',{ delay:500});
+ScrollReveal().reveal('.contactInfo education',{ delay:600});
+
+ScrollReveal().reveal('.contactInfo language',{ delay:800});
+ScrollReveal().reveal('.right_Side',{ delay:900});
